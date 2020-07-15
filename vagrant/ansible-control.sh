df4d343a6c58d5c65c75b2f891b6cfd44a078d56
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2020-07-15 18:31:26 +0200

    Update ansible-control.sh

--- vagrant/ansible-control.sh
+++ vagrant/ansible-control.sh
@@ -29,7 +29,7 @@ echo -e '192.168.56.6\n192.168.56.7' >> /etc/ansible/hosts
 sed -i 's/#host_key_checking/host_key_checking/g' /etc/ansible/ansible.cfg
 
 # Download Ansible base project
-su $ANSIBLE_USER -c 'git clone $ANSIBLE_PROJECT ~/ansible-project'
+su $ANSIBLE_USER -c "git clone $ANSIBLE_PROJECT ~/ansible-project"
 #su $ANSIBLE_USER -c "git clone https://github.com/rudibroekhuizen/ansible ~/ansible-myansibleproject"
 
 # Download Ansible roles
