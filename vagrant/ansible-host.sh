c7207108772753512b9305959264e33a57f6a301
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2021-01-13 11:15:42 +0100

    Update ansible-host.sh

--- vagrant/ansible-host.sh
+++ vagrant/ansible-host.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-ANSIBLE_USER="ansible"
+ANSIBLE_USER=ansible
 
 # Create Ansible user
 adduser --disabled-password --gecos "" $ANSIBLE_USER
 mkdir -p /home/$ANSIBLE_USER/.ssh
 echo "$ANSIBLE_USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/$ANSIBLE_USER
 
-# Manage public key
+# Add created public key to ansible user
 cat /vagrant/id_rsa.pub >> /home/$ANSIBLE_USER/.ssh/authorized_keys
 chown -R $ANSIBLE_USER:$ANSIBLE_USER /home/$ANSIBLE_USER/.ssh
 chmod 600 /home/$ANSIBLE_USER/.ssh/authorized_keys
 
-# Add public keys from host to user vagrant
+# Add existing public keys from host to user vagrant
 cat /vagrant/authorized_keys >> /home/vagrant/.ssh/authorized_keys
